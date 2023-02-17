df69fbc20a2a75dfdb6b7cc3dac7676581ce1af3
khaoula1972 <khaoula.nakkach@gmail.com>
2023-02-17 04:58:18 -0800

    my firt c print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-/*this is printable cod*/
+/**
+ * main - Entry point
+ *
+ * Description : 'This is a program is about printing a text'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
