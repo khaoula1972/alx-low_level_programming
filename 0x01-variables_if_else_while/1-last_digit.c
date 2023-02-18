3554b5be7d58ae8004e92b4d206ddb5445ddd5b9
khaoula1972 <khaoula.nakkach@gmail.com>
2023-02-18 08:30:26 -0800

    the last digit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,6 +24,5 @@ int main(void)
 		printf("Last string of %d is %d and is greater than 5\n", n, m);
 	else
 		printf("Last string of %d is %d and is less than 6 and not 0\n", n, m);
-	putchar(CR);
 	return (0);
 }
