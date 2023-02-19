e1bf7b0d86d62451827248c201078034cae345b8
khaoula1972 <khaoula.nakkach@gmail.com>
2023-02-18 16:32:37 -0800

    printing the alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a', ch = 'z', ch++)
+		putchar(ch);
+	for (ch = 'A', ch = 'B', ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
