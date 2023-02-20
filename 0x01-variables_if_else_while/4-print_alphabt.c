0a47799d1b76e373556d448e413dc3fd2eac684e
khaoula1972 <khaoula.nakkach@gmail.com>
2023-02-20 15:38:54 -0800

    the alphabtes without q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,10 +12,10 @@
 int main(void)
 {
 	int i;
-
-        for (i = 97; i <= 122; i++)
+	
+	for (i = 97; i <= 122; i++)
 	{
-		if(i != 101 && i != 113)
+		if (i != 101 && i != 113)
 			putchar(i);
 	}
 	putchar('\n');
