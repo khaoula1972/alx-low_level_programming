22b5c6ff8c08eb32ca6ba084ba010366530eeb67
khaoula1972 <khaoula.nakkach@gmail.com>
2023-02-20 15:31:33 -0800

    the alphabtes without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+        int i;
+
+        for (i = 97; i <= 122; i++)
+        {
+		if(i != 101 && i != 113)
+			putchar(i);
+	}
+	putchar('\n');
+      	return (0);
+}
