099733b636ce46b459a5af148746aa61c8023f32
khaoula1972 <khaoula.nakkach@gmail.com>
2023-02-20 18:53:49 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'this is a program about printing a text but using printf'
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
