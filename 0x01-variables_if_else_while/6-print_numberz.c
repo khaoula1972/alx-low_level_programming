21f780ce921db393c0ce5e8bfa953bd53bd3d4de
khaoula1972 <khaoula.nakkach@gmail.com>
2023-02-20 16:01:36 -0800

    number but with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	
+	for (i = 49; i <= 57; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
