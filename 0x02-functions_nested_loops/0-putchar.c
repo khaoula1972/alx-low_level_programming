db37e213a3c0b1af4953abe5af821d7cb367d385
khaoula1972 <khaoula.nakkach@gmail.com>
2023-02-22 15:06:29 -0800

    writing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include "main.h"
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
+ * is written by nbah
+ * Return: 0
 */
+
 int main(void)
 {
-        printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
