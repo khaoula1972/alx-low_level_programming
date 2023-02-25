0698f7d06a88628a58557e2822216756deff17db
khaoula1972 <khaoula.nakkach@gmail.com>
2023-02-24 18:58:27 -0800

    fizz and buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
@@ -24,6 +24,7 @@ int main(void)
 			printf("%d ", i);
 		}
 	}
+	printf("Buzz")
 	printf("\n");
 	return (0);
 }
