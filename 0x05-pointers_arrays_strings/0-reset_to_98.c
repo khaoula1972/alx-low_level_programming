08c5c2b2b80c12c5ffd6065e97653a39555feadc
khaoula1972 <khaoula.nakkach@gmail.com>
2023-02-28 13:19:03 -0800

    this resets everything to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "khaoula.h"
+
+/**
+ * reset_to_98n - takes a pointer to an int as parameter and update
+ * the value it points to to 98
+ * @n: point to an int
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
