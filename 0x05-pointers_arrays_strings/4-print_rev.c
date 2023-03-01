b34e92f1008cbe3a8c43d61fbe20b29b3b38d7b7
khaoula1972 <khaoula.nakkach@gmail.com>
2023-02-28 16:32:01 -0800

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - reverse
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	s--;
+	for (j = i; j > 0; j--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
