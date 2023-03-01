f3b3a786d09e81f9c4c227e80a310abe77c0ab8b
khaoula1972 <khaoula.nakkach@gmail.com>
2023-02-28 17:06:38 -0800

    pointers and arrays

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,11 +18,13 @@ int _atoi(char *s)
 
 	while (s[len] != '\0')
 		len++;
+
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
-		if (s[i] > '0' && s[i] <= '9')
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 			if (d % 2)
@@ -35,7 +37,10 @@ int _atoi(char *s)
 		}
 		i++;
 	}
+
 	if (f == 0)
 		return (0);
+
 	return (n);
 }
+
