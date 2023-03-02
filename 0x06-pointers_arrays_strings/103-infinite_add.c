d06d8c3fd4c1b6c44c289a7337c1aaefa2ba0d78
khaoula1972 <khaoula.nakkach@gmail.com>
2023-03-02 14:25:04 -0800

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -43,3 +43,30 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		return (0);
 	return (r + r_index + 1);
 }
+/**
+ * infinite_add - add infinitely
+ *
+ * @n1: integer
+ * @n2: integer
+ * @r: integer
+ * @size_r: integer
+ *
+ * Return: nothing
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int index, n1_len = 0, n2_len = 0;
+
+	for (index = 0; *(n1 + index); index++)
+		n1_len++;
+	for (index = 0; *(n2 + index); index++)
+		n2_len++;
+
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
+		return (0);
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
+	*(r + size_r) = '\0';
+
+	return (add_strings(n1, n2, r, --size_r));
+}
