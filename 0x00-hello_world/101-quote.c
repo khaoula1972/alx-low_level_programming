9448c46a47767b11e9ae2944faebfe8912e4e9f7
khaoula1972 <khaoula.nakkach@gmail.com>
2023-05-21 16:38:44 -0700

    no puts or printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+/**
+ * main - Entry point
+ *
+ * Description : Printing without printf or puts
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	char g[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, g, sizeof(g) - 1);
+	return (1);
+}
