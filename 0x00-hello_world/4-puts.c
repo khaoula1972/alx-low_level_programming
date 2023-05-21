9d77a45ba08e9108a29d8815d55cd7d6558a98d2
khaoula1972 <khaoula.nakkach@gmail.com>
2023-05-21 15:23:20 -0700

    First puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 /*
- *main - Entry point of the program
- *Description: here's a simple programm
+ * main - Entry point of the program
+ * Description : here's a simple programm
  *		that prints a phrase.
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 #include <stdio.h>
 int main(void)
