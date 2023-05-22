827d6acf9f6fd28e7958a02375665291471050b3
khaoula1972 <khaoula.nakkach@gmail.com>
2023-05-21 18:40:32 -0700

    low/up case alphabet with putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description : 'Calling the alphabet game low/up case'
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	ch = 'A';
+	while (ch <= 'Z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
