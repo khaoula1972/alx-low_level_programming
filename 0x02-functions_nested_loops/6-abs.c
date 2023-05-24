817bff88434b7008d5bd558bdbc616d4998ca774
khaoula1972 <khaoula.nakkach@gmail.com>
2023-05-23 19:30:41 -0700

    Absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @n: interger
+ * Description : 'Absolute value of a number'
+ * Return: the absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
