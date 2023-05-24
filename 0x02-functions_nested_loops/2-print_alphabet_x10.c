120224264f777e066fe39521efc53083d2c02e43
khaoula1972 <khaoula.nakkach@gmail.com>
2023-05-23 18:45:59 -0700

    Printing alphabets 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include "1-alphabet.c"
 /**
- *  * print_alphabet_x10 - Entry point
- *   * Description : 'Printing the alphabet'
- *     */
+ * print_alphabet_x10 - Entry point
+ * Description : 'Printing the alphabet'
+ */
 void print_alphabet_x10(void)
 {
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		print_alphabet();
