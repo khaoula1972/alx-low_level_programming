1fb9e754e2354c7195a9d3b493fc7df3a9202550
khaoula1972 <khaoula.nakkach@gmail.com>
2023-05-23 18:20:11 -0700

    header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,17 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c)
-void print_alphabet(void);
-void print_alphabet_x10(void);
+int _putchar(char c);
+void print_alphabet();
+void print_alphabet_x10();
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer();
+void times_table();
+int add(int b, int a);
 void print_to_98(int n);
 
 #endif
