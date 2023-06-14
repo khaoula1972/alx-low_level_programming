e5beb7c83679995a557d7b211675e05aef31e625
khaoula1972 <khaoula.nakkach@gmail.com>
2023-06-14 08:29:39 -0700

    printing arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Prints all the arguments received
+ * @argc: The argument count
+ * @argv: The argument vector
+ *
+ * Return: Always 0
+ */
 
 int main(int argc, char *argv[])
 {
