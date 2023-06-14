eed99f2c6ad729f3bbd5988807c54cd2890bb2c4
khaoula1972 <khaoula.nakkach@gmail.com>
2023-06-14 10:12:45 -0700

    multiplaction of two nbrs

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that multiplies two numbers.
+ * @argc: The argument count
+ * @argv: The argument vector
+ * Return: 1 if ther'es error, the results if not
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	i = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", i);
+	return (1);
+}
