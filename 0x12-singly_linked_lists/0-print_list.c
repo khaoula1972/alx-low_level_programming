88586489256c5a7154a1e82088083f2d50c50ee1
root <root@DESKTOP-UVRPB7U.localdomain>
2023-07-01 17:18:24 +0100

    print linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "lists.h"
 /**
  * print_list - a function that prints all the elements of a list_t list.
  * @h: a pointer to the header of the linked list
@@ -6,12 +6,12 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t length;
+	size_t length = 0;
 
 	while (h != NULL)
 	{
 		if (h->str != NULL)
-			printf("[%lu] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
 		length++;
