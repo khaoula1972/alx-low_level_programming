29f551135c43408d2452f1b5c6e9313e8a3519d6
khaoula1972 <khaoula.nakkach@gmail.com>
2023-07-01 18:24:54 -0700

    write combination of numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,8 +14,9 @@ int main(void)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-			putchar(44);
-			putchar(32);
+			if (i != 8 && j != 9)
+				putchar(44);
+				putchar(32);
 		}
 	}
 	putchar('\n');
