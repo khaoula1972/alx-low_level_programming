3b28bcfa32f264f48714fbfbdea403cca5dda16c
khaoula1972 <khaoula.nakkach@gmail.com>
2023-07-04 22:07:34 +0100

    get node by index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,7 +2,7 @@
 /**
  * get_nodeint_at_index - a function that  returns the nth node
  * of a listint_t linked list.
- * @h: a pointer to the header of the linked list
+ * @head: a pointer to the header of the linked list
  * @index: the index of the node to be returned
  * Return: the number of nodes.
  */
