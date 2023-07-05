0111675ed85f6b78227278e66113850e396a80f7
Khaoula Nakkach <123691690+khaoula1972@users.noreply.github.com>
2023-07-05 01:37:55 +0100

    printing a llinked list safely

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,7 +12,7 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *current = head, *loop_node = NULL;
 
 	if (head == NULL)
-		exit(98);
+		return (0);
 
 	while (current != NULL)
 	{
