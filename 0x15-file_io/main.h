ca1f0a89ea134c4e1181cadde318a3f041cbe6de
khaoula1972 <khaoula.nakkach@gmail.com>
2023-07-11 04:18:30 +0100

    head file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,22 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+
+#define BUF_SIZE 1024
+
+/**
+ * error_exit - Prints an error message
+ * @msg: The error message to print
+ * @exit_code: The exit code to return
+ */
+void error_exit(const char *msg, int exit_code)
+{
+	while (*msg)
+		putchar(*msg++);
+	putchar('\n');
+	exit(exit_code);
+}
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
