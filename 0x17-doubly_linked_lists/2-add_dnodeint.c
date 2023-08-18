fd3f88e077c45a9194a4f7a1c7bc4ef45b34a43c
khaoula1972 <khaoula.nakkach@gmail.com>
2023-08-17 18:50:19 -0700

    add node ad begining

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,7 +2,8 @@
 /**
  * add_dnodeint - a function that adds a new node
  * at the beginning of the list
- * @h: the head
+ * @head: the head
+ * @n: the value of the new node
  * Return: the address of the new element,
  * or NULL if it failed
  */
@@ -25,8 +26,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (*head != NULL)
 		(*head)->prev = new;
-	
+
 	*head = new;
-	
+
 	return (new);
 }
