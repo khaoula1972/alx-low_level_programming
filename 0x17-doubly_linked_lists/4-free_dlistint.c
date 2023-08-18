cf33577625a839961aaf1708b79f60ad7d031b56
khaoula1972 <khaoula.nakkach@gmail.com>
2023-08-17 19:10:31 -0700

    free double linked list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,11 +2,10 @@
 /**
  * free_dlistint - a function that frees a dlistint_t list.
  * @head: the head
- * Return: the number of nodes
  */
 void free_dlistint(dlistint_t *head)
 {
-	const dlistint_t *current, *temp;
+	dlistint_t *current, *temp;
 
 	if (head == NULL)
 		return;
