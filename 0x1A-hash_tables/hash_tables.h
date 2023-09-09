557e0730c1350a2295e5ff0b14c696f1363dae10
khaoula1972 <khaoula.nakkach@gmail.com>
2023-09-09 04:37:02 +0100

    header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -41,5 +42,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+
+/* Helpful functions */
+static hash_node_t *create_node(const char *key, const char *value);
 
 #endif
